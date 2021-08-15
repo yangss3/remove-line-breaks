ababa63d79a41a8dc8c8dfcda6cdf04de7764490
yangss3 <yss_2016@outlook.com>
2021-08-15 14:19:04 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import clipboardy from 'clipboardy'
+
+async function main () {
+  console.log('------Waiting for change of your clipboard------')
+  let prev = clipboardy.readSync()
+  setInterval(async () => {
+    const text = await clipboardy.read()
+    if (text !== prev) {
+      prev = text.replace(/\r\n/g, ' ')
+      clipboardy.writeSync(prev)
+      const len = prev.length
+      console.log(`All line breaks in "${
+        len < 40 ? prev : `${prev.slice(0, 15)}...${prev.slice(-15)}`
+      }" has been removed.`)
+      console.log('------Waiting for change of your clipboard------')
+    }
+  }, 1000)
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(0)
+})
