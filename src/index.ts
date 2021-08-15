18d10931d6bfef6db350cdb4f474130149d8a5af
yangss3 <yss_2016@outlook.com>
2021-08-15 20:29:53 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,18 @@ async function main () {
   console.log('------Waiting for changes from your clipboard------')
   let prev = clipboardy.readSync()
   setInterval(async () => {
-    const text = await clipboardy.read()
-    if (text !== prev) {
-      prev = text.replace(/\r\n/g, ' ')
-      clipboardy.writeSync(prev)
-      const len = prev.length
-      console.log(`All line breaks in "${
-        len < 40 ? prev : `${prev.slice(0, 15)}...${prev.slice(-15)}`
-      }" has been removed.`)
-      console.log('------Waiting for changes from your clipboard------')
-    }
+    try {
+      const text = await clipboardy.read()
+      if (text !== prev) {
+        prev = text.replace(/\r\n/g, ' ')
+        clipboardy.writeSync(prev)
+        const len = prev.length
+        console.log(`All line breaks in "${
+          len < 40 ? prev : `${prev.slice(0, 15)}...${prev.slice(-15)}`
+        }" has been removed.`)
+        console.log('------Waiting for changes from your clipboard------')
+      }
+    } catch (err) {}
   }, 1000)
 }
 
